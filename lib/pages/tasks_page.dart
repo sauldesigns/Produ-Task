8fd004e3daa12869f23352471d4dcc88a615d16e
Saul Designs <sauldesigns@Sauls-MBP.frontierlocal.net>
2019-06-26 02:23:58 -0700

    edited padding

--- lib/pages/tasks_page.dart
+++ lib/pages/tasks_page.dart
@@ -25,81 +25,79 @@ class _TasksPageState extends State<TasksPage> {
       body: StreamProvider<List<Task>>.value(
         value: db.categoryTasks(_user, widget.category),
         child: StreamBuilder<Object>(
-          stream: db.streamHero(_user.uid),
-          builder: (context, snapshot) {
-            User _userDb = snapshot.data;
-            List<Task> task = Provider.of<List<Task>>(context);
-            return SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      top: 65,
-                      bottom: 0,
-                      left: 20,
-                      right: 20,
-                    ),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        IconButton(
-                          icon: Icon(Icons.arrow_back),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                        Expanded(
-                          child: Container(),
-                        ),
-                        ProfilePicture(
-                          size: 25,
-                          imgUrl: _userDb == null ? null : _userDb.profilePic,
-                        )
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 40.0, left: 40.0, right: 40.0),
-                    child: Text(
-                      '${widget.category.title}',
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.w500,
+            stream: db.streamHero(_user.uid),
+            builder: (context, snapshot) {
+              User _userDb = snapshot.data;
+              List<Task> task = Provider.of<List<Task>>(context);
+              return SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 65,
+                        bottom: 0,
+                        left: 20,
+                        right: 20,
+                      ),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          IconButton(
+                            icon: Icon(Icons.arrow_back),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          Expanded(
+                            child: Container(),
+                          ),
+                          ProfilePicture(
+                            size: 25,
+                            imgUrl: _userDb == null ? null : _userDb.profilePic,
+                          )
+                        ],
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 40.0, right: 40.0, top: 15.0, bottom: 40.0),
-                    child: Text(
-                      '${task == null ? 0 : task.length} tasks',
-                      style: TextStyle(
-                        fontSize: 17,
-                    
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          top: 40.0, left: 40.0, right: 40.0),
+                      child: Text(
+                        '${widget.category.title}',
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
                     ),
-                  ),
-                  
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20.0, right: 20),
-                    child: ListView.builder(
-                      itemCount: task == null ? 0 : task.length,
-                      physics: ScrollPhysics(),
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) {
-                        Task taskData = task[index];
-                        return ListTile(
-                          title: Text(taskData.title),
-
-                        );
-                      },
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 40.0, right: 40.0, top: 15.0, bottom: 0.0),
+                      child: Text(
+                        '${task == null ? 0 : task.length} tasks',
+                        style: TextStyle(
+                          fontSize: 17,
+                        ),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            );
-          }
-        ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20.0, right: 20),
+                      child: ListView.builder(
+                        itemCount: task == null ? 0 : task.length,
+                        physics: ScrollPhysics(),
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          Task taskData = task[index];
+                          return ListTile(
+                            title: Text(taskData.title),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }),
       ),
     );
   }
