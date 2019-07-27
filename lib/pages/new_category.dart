0caf073e4889cb79dbc1060550dc9ed9108619a3
Saul Designs <saul.pla1221@gmail.com>
2019-07-26 22:54:55 -0700

    added color selector when creating new category

--- lib/pages/new_category.dart
+++ lib/pages/new_category.dart
@@ -31,6 +31,13 @@ class _NewCategoryState extends State<NewCategory> {
   final Firestore _db = Firestore.instance;
   bool _autoValidate = false;
   String _category;
+  int _colorIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    _colorIndex = widget.colorIndex;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,113 +55,140 @@ class _NewCategoryState extends State<NewCategory> {
         height: MediaQuery.of(context).size.height,
         child: SingleChildScrollView(
           physics: AlwaysScrollableScrollPhysics(),
-          child: Padding(
-            padding: const EdgeInsets.only(left: 50.0, right: 50.0),
-            child: Form(
-              key: _formkey,
-              autovalidate: _autoValidate,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(top: 30.0, bottom: 40),
-                    child: RichText(
-                      text: TextSpan(
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 22,
-                            fontWeight: FontWeight.w500,
-                          ),
-                          children: <TextSpan>[
-                            TextSpan(
-                              text: widget.update == false
-                                  ? 'Create a new '
-                                  : 'Update ',
-                              style: TextStyle(
-                                fontSize: 25,
-                                fontWeight: FontWeight.w300,
-                              ),
-                            ),
-                            TextSpan(
-                              text: 'Category',
-                              style: TextStyle(
-                                fontSize: 25,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ]),
-                    ),
-                  ),
-                  Center(
-                    child: Hero(
-                      tag: widget.catID,
-                      child: Material(
-                        child: CustomCard(
-                          blurRadius: 10,
-                          width: 250,
-                          height: 250,
-                          color: widget.listColors[widget.colorIndex],
-                          title: TextFormField(
-                            textCapitalization: TextCapitalization.sentences,
-                            autofocus: true,
-                            cursorColor: Colors.white,
-                            initialValue: widget.initialText,
-                            textAlign: TextAlign.center,
+          child: Form(
+            key: _formkey,
+            autovalidate: _autoValidate,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 50.0, right: 50.0, top: 30.0, bottom: 40),
+                  child: RichText(
+                    text: TextSpan(
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: widget.update == false
+                                ? 'Create a new '
+                                : 'Update ',
                             style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
+                              fontSize: 25,
+                              fontWeight: FontWeight.w300,
                             ),
-                            keyboardType: TextInputType.text,
-                            maxLines: 4,
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hasFloatingPlaceholder: false,
-                                hintText: 'Enter Text Here'),
-                            validator: (value) {
-                              if (value.isEmpty) {
-                                return 'Must enter some text';
-                              }
-                              return null;
-                            },
-                            onSaved: (value) => _category = value,
                           ),
+                          TextSpan(
+                            text: 'Category',
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ]),
+                  ),
+                ),
+                Center(
+                  child: Hero(
+                    tag: widget.catID,
+                    child: Material(
+                      child: CustomCard(
+                        blurRadius: 10,
+                        width: 250,
+                        height: 250,
+                        color: widget.listColors[_colorIndex],
+                        title: TextFormField(
+                          textCapitalization: TextCapitalization.sentences,
+                          autofocus: true,
+                          cursorColor: Colors.white,
+                          initialValue: widget.initialText,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                          keyboardType: TextInputType.text,
+                          maxLines: 4,
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hasFloatingPlaceholder: false,
+                              hintText: 'Enter Text Here'),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Must enter some text';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) => _category = value,
                         ),
                       ),
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 30),
-                    child: Center(
-                      child: RoundedButton(
-                        title: widget.update == false
-                            ? 'Create Category'
-                            : 'Update Category',
-                        onClick: () {
-                          if (hasVibration) {
-                            Vibration.vibrate(duration: 200);
-                          }
-                          _formkey.currentState.save();
-                          if (_formkey.currentState.validate()) {
-                            var data = {
-                              'content': _category,
-                              'done': true,
-                            };
-                            _db
-                                .collection('category')
-                                .document(widget.catID)
-                                .updateData(data);
-                            Navigator.of(context).pop();
-                          } else {
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 40, bottom: 10),
+                  child: Container(
+                    height: 40,
+                    child: ListView.builder(
+                      itemCount: widget.listColors.length,
+                      scrollDirection: Axis.horizontal,
+                      padding: EdgeInsets.only(left: 10),
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          splashColor: Colors.transparent,
+                          highlightColor: Colors.transparent,
+                          onTap: () {
                             setState(() {
-                              _autoValidate = true;
+                              _colorIndex = index;
                             });
-                          }
-                        },
-                      ),
+                          },
+                          child: Padding(
+                            padding: EdgeInsets.only(right: 15),
+                            child: CircleAvatar(
+                              backgroundColor: widget.listColors[index],
+                            ),
+                          ),
+                        );
+                      },
                     ),
-                  )
-                ],
-              ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(right: 50.0, left: 50.0, top: 30),
+                  child: Center(
+                    child: RoundedButton(
+                      title: widget.update == false
+                          ? 'Create Category'
+                          : 'Update Category',
+                      onClick: () {
+                        if (hasVibration) {
+                          Vibration.vibrate(duration: 200);
+                        }
+                        _formkey.currentState.save();
+                        if (_formkey.currentState.validate()) {
+                          var data = {
+                            'content': _category,
+                            'color': _colorIndex,
+                            'done': true,
+                          };
+                          _db
+                              .collection('category')
+                              .document(widget.catID)
+                              .updateData(data);
+                          Navigator.of(context).pop();
+                        } else {
+                          setState(() {
+                            _autoValidate = true;
+                          });
+                        }
+                      },
+                    ),
+                  ),
+                )
+              ],
             ),
           ),
         ),
